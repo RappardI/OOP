dce41c8ae80b6a27a683a0b8b2ccce5e367b34c6
RappardI <isaac.rappard@gmail.com>
2026-08-28 09:12:27 -0500

    Add files via upload

---
+++ OOP-1.py
@@ -0,0 +1,12 @@
+print("Employee name is Isaac")
+name = input
+
+print("Enter the value of basicpay:")
+basicpay= int(input())
+
+print("Enter the value of deductions:")
+deductions = int(input())
+
+totalpay = (basicpay-deductions)
+
+print("The total pay is", totalpay)
