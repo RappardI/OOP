00d013a71e3ca2c559024b33eb3f6a1a70a728bb
RappardI <isaac.rappard@gmail.com>
2026-09-16 09:25:00 -0500

    Update list manipulation options and logic
    Refactor menu options and improve list operations.

--- ClassWork-02.py
+++ ClassWork-02.py
@@ -1,24 +1,24 @@
 while("true"):
-    print("1 Addition")
-    print("2 Subtraction")
-    print("3 Multiplication")
-    print("4 Division")
+    print("1 Replace a number")
+    print("2 Remove a number")
+    print("3 Add a number")
     choice = input("Enter your choice: ")
 
 
     mylist = [5,10,15,20,25,30]
     print("current list is -",mylist)
 
-    mylist.append(int(input("enter number to be added to list")))
-
-    mylist.remove(int(input("enter number to be removed from list:")))
+    if new_remove = int(input())
     new_remove = int(input())
-    if new_remove in mylist:
-    print("element is in the list")
     elif new_remove in mylist:
-
+    mylist.remove(new_remove)
+    elif choice == "1":
     mylist.remove(int(input("enter number to be replaced in list:")))
     mylist.append(int(input("enter the number you want it to replaced it with:")))
+    elif choice == "2":
+    mylist.append(int(input("enter number to be added to list")))
+    elif choice == "3":
+    mylist.remove(int(input("enter number to be removed from list:")))
 
     mylist.sort()
     print(mylist)
