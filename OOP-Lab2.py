1054b83bd62be01fbc95942b5fc49069a7245088
RappardI <isaac.rappard@gmail.com>
2026-09-25 15:36:44 -0500

    Refactor employee management functions
    Refactored employee management functions to calculate gross and net pay automatically. Improved user prompts and added confirmation messages for actions.

--- OOP-Lab2.py
+++ OOP-Lab2.py
@@ -1,64 +1,81 @@
 myEmployees = {}
-i=1
 
 def add_employee():
-    employee_name = input("enter employee name")
+    employee_name=input("enter employee name")
+    basic_pay=int(input("enter basic pay"))
+    allowance=int(input("enter allowance"))
+    deductions=int(input("enter deductions"))
+    taxes=int(input("enter taxes"))
 
+    gross_pay = basic_pay + allowance
+    net_pay = gross_pay - deductions - taxes
 
-        basic_pay=int(input("enter basic pay"))
-        allowance=int(input("enter allowance"))
-        deductions=int(input("enter deductions"))
-        taxes=int(input("enter taxes"))
-        gross_pay=int(input("enter gross_pay"))
-        net_pay=int(input("enter net_pay"))
-        myEmployees.update({"E"+str(i):
-            {   "e_name": employee_name,
-                "basic_pay": basic_pay,
-                "allowance": allowance,
-                "deductions": deductions,
-                "taxes": taxes,
-                "gross_pay": gross_pay,
-                "net_pay": net_pay
-                }})
+    myEmployees[employee_name] = {
+            "e_name": employee_name,
+            "basic_pay": basic_pay,
+            "allowance": allowance,
+            "deductions": deductions,
+            "taxes": taxes,
+            "gross_pay": gross_pay,
+            "net_pay": net_pay
+    }
+    print("Employee has been added")
 
 
 
 def delete_employee():
     employee_name = input("enter employee name")
     if employee_name in myEmployees:
-        myEmployees.pop(employee_name)
+       del myEmployees[employee_name]
+       print("Employee deleted")
     else:
-        print("employee does not exist")
-
-    def modify_employee():
-        empid = input("enter employee id")
-
-        updated_employee_name= input("enter employee name")
-        myEmployees[empid]["e_name"] = updated_employee_name
-        updated_employee_basic_pay= input("enter employee name")
-        myEmployees[empid]["basic_pay"] = updated_employee_basic_pay
-        updated_employee_allowance= input("enter employee name")
-        myEmployees[empid]["allowance"] = updated_employee_allowance
-        updated_employee_deductions= input("enter employee deductions")
-        myEmployees[empid]["deductions"] = updated_employee_deductions
-        updated_employee_taxes= input("enter employee taxes")
-        myEmployees[empid]["taxes"] = updated_employee_taxes
-        updated_employee_gross_pay= input("enter employee gross pay")
-        myEmployees[empid]["gross_pay"] = updated_employee_gross_pay
-        updated_employee_net_pay= input("enter employee net pay")
-        myEmployees[empid]["net_pay"] = updated_employee_net_pay
+        print("Employee does not exist")
 
 
+def modify_employee():
+    employee_name = input("enter employee name")
+    if employee_name in myEmployees:
+        basic_pay=int(input("enter basic pay"))
+        allowance =int(input("enter allowance"))
+        deductions =int(input("enter deductions"))
+        taxes =int(input("enter taxes"))
+
+        gross_pay = basic_pay + allowance
+        net_pay = gross_pay - deductions - taxes
+
+        myEmployees[employee_name] = {
+            "e_name": employee_name,
+            "basic_pay": basic_pay,
+            "allowance": allowance,
+            "deductions": deductions,
+            "taxes": taxes,
+            "gross_pay": gross_pay,
+            "net_pay": net_pay
+        }
+        print("Employee has been modified")
+    else:
+        print("Employee does not exist")
 
 def display_employees():
     print(myEmployees)
 
 
-
 while True:
-    print menu
+    print ("1. Add an employee")
+    print ("2. Delete an employee")
+    print ("3. Modify an employee")
+    print ("4. Display all employees")
+    print ("5. Exit")
 
+    choice = input("Enter your choice")
 
-
-    add_employee()
-    i=i+1
+    if choice == "1":
+        add_employee()
+    elif choice == "2":
+        delete_employee()
+    elif choice == "3":
+        modify_employee()
+    elif choice == "4":
+        display_employees()
+    elif choice == "5":
+        break
